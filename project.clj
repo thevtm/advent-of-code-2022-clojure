036bf55fd506e3685dc65bdd0210207bd03783d3
Vinicius T. Manjabosco <978715+thevtm@users.noreply.github.com>
2023-03-06 23:48:14 +0100

    Add DataScript

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [datascript "1.3.15"]]
   :main ^:skip-aot advent-of-code-2022.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
