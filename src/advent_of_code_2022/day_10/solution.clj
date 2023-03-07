7e9fa47394f588a23e793205b471df142c05c388
Vinicius T. Manjabosco <978715+thevtm@users.noreply.github.com>
2023-03-07 21:08:55 +0100

    Day 10 - Part 2

--- src/advent_of_code_2022/day_10/solution.clj
+++ src/advent_of_code_2022/day_10/solution.clj
@@ -1,9 +1,8 @@
 (ns advent-of-code-2022.day-10.solution
   (:require [clojure.string :as str]))
 
-;; (def input-text (slurp "./src/advent_of_code_2022/day_10/input.txt"))
-(def input-text (slurp "./src/advent_of_code_2022/day_10/sample.txt"))
-;; (def input-text (slurp "./src/advent_of_code_2022/day_09/sample-part-2.txt"))
+(def input-text (slurp "./src/advent_of_code_2022/day_10/input.txt"))
+;; (def input-text (slurp "./src/advent_of_code_2022/day_10/sample.txt"))
 
 ;; Lines
 
@@ -28,3 +27,18 @@
 
 ;; Answer 1: 10760
 (reduce + (map #(* % (nth cycle-x-values %)) [20 60 100 140 180 220]))
+
+;; Answer 2: FPGPHFGH
+(def cycle-x-value-sprite-x-positions
+  (vec (map #(set [(dec %) % (inc %)]) (drop 1 cycle-x-values))))
+(comment (drop 0 (map list (range) cycle-x-values cycle-x-value-sprite-x-positions)))
+
+(def cycle-cursor-x-positions (map #(mod % 40) (range)))
+
+(def screen-pixels
+  (->> (map contains? cycle-x-value-sprite-x-positions cycle-cursor-x-positions)
+       (map #(if % "#" "."))))
+
+(->> screen-pixels
+     (partition 40)
+     (map str/join))
